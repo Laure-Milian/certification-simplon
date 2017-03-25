e8cc7f67312cc3a062f61fc2ed7565e2626267e6
marineco <marinecolonge@gmail.com>
2017-03-25 23:33:30 +0100

    ajout emplacement nom du site

---
+++ resources/views/accueil.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Affichage des Modules</title>
+  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/bootstrap/4.0.0-alpha.6/css/bootstrap.css">
+  <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+  <!-- HEADER -->
+  <!-- Nom du site -->
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-3">
+        <h2 class="site">Le trésor des loutres pirates</h2>
+      </div>
+    </div>
+  </div>
+
+
+  <script type="text/javascript" src="/js/app.js"></script>
+</body>
+</html>
