6384b393746800197477d45f26e9d2c383e6059e
marineco <marinecolonge@gmail.com>
2017-03-25 23:53:19 +0100

    ajout des blocs photos 1 et 2

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -3,13 +3,16 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Affichage des Modules</title>
+  <title>Le Trésor des Loutres Pirates</title>
   <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/bootstrap/4.0.0-alpha.6/css/bootstrap.css">
   <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
+
   <!-- HEADER -->
+
   <!-- Nom du site -->
+
   <div class="container-fluid">
     <div class="row">
       <div class="col-3">
@@ -41,22 +44,59 @@
            </div>
          </form>
        </div>
-     </div>
+      </div>
 
-     <!-- Mon espace + s'inscrire -->
+      <!-- Mon espace + s'inscrire -->
 
-     <div class="col-3">
-      <div class="loginPart">
-        <form class="form-inline pull-xs-right signUp">
-          <input id="email" type="email" class="form-control" name="email" value="" placeholder="Mail"> 
-          <input id="password" type="password" class="form-control" name="password" value="" placeholder="Mot de passe">
-          <a class="btn btn-primary" href="#"><i class="fa fa-sign-in fa-lg fa-fw"></i>Login</a>
-        </form>
+       <div class="col-3">
+        <div class="loginPart">
+          <form class="form-inline pull-xs-right signUp">
+            <input id="email" type="email" class="form-control" name="email" value="" placeholder="Mail"> 
+            <input id="password" type="password" class="form-control" name="password" value="" placeholder="Mot de passe">
+            <a class="btn btn-primary" href="#"><i class="fa fa-sign-in fa-lg fa-fw"></i>Login</a>
+          </form>
+        </div>
       </div>
     </div>
   </div>
-</div>
+  
+  <!-- FIN HEADER -->
 
+  <!-- BLOC 1 : grosse image à gauche 2 images à droite -->
+  <div class="container-fluid">
+    <div class="row bloc1">
+      <div class="gallery_product col col-md-8 filter hdpe">
+        <img src="images/escarpins.jpg" class="img-responsive">
+      </div>
+      <div class="column col-6 col-md-4 filter hdpe">
+        <div class="gallery_product">
+          <img src="images/baskets.jpg" class="img-responsive">
+          <img src="images/bottines.jpg" class="img-responsive">
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- BLOC 2 : 3 images  -->
+  <div class="container">
+    <div class="row bloc2">
+      <div class="gallery col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <h1 class="gallery-title">Notre sélection</h1>
+      </div>
+
+      <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+        <img src="images/escarpins.jpg" class="img-responsive">
+      </div>
+
+      <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter sprinkle">
+        <img src="images/baskets.jpg" class="img-responsive">
+      </div>
+
+      <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+        <img src="images/bottines.jpg" class="img-responsive">
+      </div>
+    </div>
+  </div>
 
 <script type="text/javascript" src="/js/app.js"></script>
 </body>
