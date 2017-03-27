d44bb3fd3d45d68aa5240d7146f8afa141291486
marineco <marinecolonge@gmail.com>
2017-03-27 21:51:45 +0200

    création d'un groupe de Route pour l'administration + ajout route pr le controleur de resources des tags

---
+++ app/Http/routes.php
@@ -0,0 +1,10 @@
+<?php
+
+Route::group([
+    'prefix' => config('backpack.base.route_prefix', 'admin'),
+    'middleware' => ['admin'],
+    'namespace' => 'Admin'
+], function() {
+    // your CRUD resources and other admin routes here
+    CRUD::resource('tag', 'TagCrudController');
+}); 
