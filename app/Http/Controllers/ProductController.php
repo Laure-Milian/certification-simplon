bc5bb52ba7d9f8795e2af3bf4293f06d40eaa141
Juliane Blier <blier.juliane@gmail.com>
2017-03-29 15:56:14 +0200

    Ajout fonction qui récupère la catégorie produit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Product;
+use App\Category;
 
 class ProductController extends Controller
 {
@@ -14,12 +15,18 @@ class ProductController extends Controller
 
     public function getProduct($id) {
     	$product = Product::findOrFail($id);
-    	return view('products.details', ['product' => $product]);
+        $category = $this->getProductCategory($product->category_id);
+    	return view('products.details', ['product' => $product, 'category' => $category]);
     }
 
     public function findProduct(Request $request) {
     	$products = Product::where('name', 'like', $request->search . '%')->get();
     	return view('accueil', ['products' => $products]);
+    }
+
+    public function getProductCategory($category_id){
+        $category = Category::where('id', $category_id)->first();
+        return $category->name;
     }
 
 }
