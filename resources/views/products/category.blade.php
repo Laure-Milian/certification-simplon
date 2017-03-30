8f8560c182fdfb50a6ef82a139191fe926154dca
Juliane Blier <blier.juliane@gmail.com>
2017-03-30 17:37:23 +0200

    Page Category annonce si produit disponible ou non

--- resources/views/products/category.blade.php
+++ resources/views/products/category.blade.php
@@ -16,12 +16,20 @@
                     </div>
                     <div class="caption caption-card">
                         <h3 class="category-product-title"><a href="/{{ $product->id }}">{{ $product->name }}</a></h3>
-                        <h4 class="pull-left"><strong>{{ $product->price / 100 }} €</strong></h4>
-                        <form action="/add/cart" method="post" class="pull-right">
-                            <input type="hidden" name="product_id" value="{{$product->id}}">
-                            <input type="hidden" name="quantity" value="1">
-                            <button class="btn btn-success" type="submit"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Ajouter au panier</button>
-                        </form>
+                        
+                        <div class="category-product-stock">
+                            <h4 class="pull-left"><strong>{{ $product->price / 100 }} €</strong></h4>
+                            @if ($product->stock === 0)
+                              <div class="text-danger pull-right">Indisponible</div>
+                            @else
+                              <form action="/add/cart" method="post" class="pull-right">
+                                <input type="hidden" name="product_id" value="{{$product->id}}">
+                                <input type="hidden" name="quantity" value="1">
+                                <button class="btn btn-success" type="submit"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Ajouter au panier</button>
+                              </form>
+                            @endif
+                        </div>
+
                     </div>
                 </div>
             </div>
