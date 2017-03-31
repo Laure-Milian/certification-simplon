81a53cd0b9775da1b5262a6b69d3c99f363b2576
simplon <laure.milian@gmail.com>
2017-03-31 22:17:49 +0200

    css page account

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -4,77 +4,81 @@
 
 <div class="container">
 	<div>
-		<h1 class="order-h1"><strong>Votre compte</strong></h1>
+		<h1 class="account-h1"><strong>Votre compte</strong></h1>
 	</div>
 	<div class="panel panel-default">
-		<h2 class="order_h2">Commande(s) en cours</h2>
-		@if ($current_orders->isEmpty())
-		<h3>Vous n'avez pas de commande en cours</h3>
-		@else
-		<table class="table table-bordered">
-			<tr class="active">
-				<th>N° de commande</th>
-				<th>Nom du destinataire</th>
-				<th>Adresse de livraison</th>
-				<th>Méthode de livraison</th>
-				<th>Coût total de la commande</th>
-				<th>Commande payée</th>
-				<th>Commande envoyée</th>
-				<th>Commande reçue</th>
-			</tr>
-			@foreach ($current_orders as $current_order)
-			<tr>
-				<td>{{$current_order->id}}</td>
-				<td>{{$current_order->last_name}} {{$current_order->first_name}}</td>
-				<td>{{$current_order->address}}</td>
-				<td>{{$current_order->shipping_method}}</td>
-				<td>{{$current_order->total_price / 100}} €</td>
-				@if ($current_order->is_paid)
-				<td><span class="glyphicon glyphicon-ok text-success lead" aria-hidden="true"></span></td>
-				@else
-				<td><span class="glyphicon glyphicon-remove text-danger lead" aria-hidden="true"></span></td>
-				@endif
-				@if ($current_order->is_sent)
-				<td><span class="glyphicon glyphicon-ok text-success lead" aria-hidden="true"></span></td>
-				@else
-				<td><span class="glyphicon glyphicon-remove text-danger lead" aria-hidden="true"></span></td>
-				@endif
-				@if ($current_order->is_delivered)
-				<td><span class="glyphicon glyphicon-ok text-success lead" aria-hidden="true"></span></td>
-				@else
-				<td><span class="glyphicon glyphicon-remove text-danger lead" aria-hidden="true"></span></td>
-				@endif
-			</tr>
-			@endforeach
-		</table>
-		@endif
+		<div class="account-div">
+			<h2 class="account-h2">Commande(s) en cours</h2>
+			@if ($current_orders->isEmpty())
+			<h3 class="account-h3">Vous n'avez pas de commande en cours</h3>
+			@else
+			<table class="table table-bordered">
+				<tr class="active">
+					<th>N° de commande</th>
+					<th>Nom du destinataire</th>
+					<th>Adresse de livraison</th>
+					<th>Méthode de livraison</th>
+					<th>Coût total de la commande</th>
+					<th>Commande payée</th>
+					<th>Commande envoyée</th>
+					<th>Commande reçue</th>
+				</tr>
+				@foreach ($current_orders as $current_order)
+				<tr>
+					<td>{{$current_order->id}}</td>
+					<td>{{$current_order->last_name}} {{$current_order->first_name}}</td>
+					<td>{{$current_order->address}}</td>
+					<td>{{$current_order->shipping_method}}</td>
+					<td>{{$current_order->total_price / 100}} €</td>
+					@if ($current_order->is_paid)
+					<td class="center-table-account"><span class="glyphicon glyphicon-ok text-success lead" aria-hidden="true"></span></td>
+					@else
+					<td class="center-table-account"><span class="glyphicon glyphicon-remove text-danger lead" aria-hidden="true"></span></td>
+					@endif
+					@if ($current_order->is_sent)
+					<td class="center-table-account"><span class="glyphicon glyphicon-ok text-success lead" aria-hidden="true"></span></td>
+					@else
+					<td class="center-table-account"><span class="glyphicon glyphicon-remove text-danger lead" aria-hidden="true"></span></td>
+					@endif
+					@if ($current_order->is_delivered)
+					<td class="center-table-account"><span class="glyphicon glyphicon-ok text-success lead" aria-hidden="true"></span></td>
+					@else
+					<td class="center-table-account"><span class="glyphicon glyphicon-remove text-danger lead" aria-hidden="true"></span></td>
+					@endif
+				</tr>
+				@endforeach
+			</table>
+			@endif
+		</div>
 	</div>
 	<div class="panel panel-default">
-		<h2 class="order_h2">Historique des commandes</h2>
-		@if ($past_orders->isEmpty())
-		<h3>Vous n'avez pas de commande passées</h3>
-		@else
-		<table class="table table-bordered">
-			<tr class="active">
-				<th>N° de commande</th>
-				<th>Nom du destinataire</th>
-				<th>Adresse de livraison</th>
-				<th>Méthode de livriason</th>
-				<th>Coût total de la commande</th>
-				<th>Reçue le</th>
-			</tr>
-			@foreach ($past_orders as $past_order)
-			<tr>
-				<td>{{$past_order->id}}</td>
-				<td>{{$past_order->last_name}} {{$past_order->first_name}}</td>
-				<td>{{$past_order->address}}</td>
-				<td>{{$past_order->shipping_method}}</td>
-				<td>{{$past_order->total_price / 100}} €</td>
-				<td>{{$past_order->delivery_date / 100}}</td>
-			</tr>
-			@endforeach
-		</table>
-		@endif
+		<div class="account-div">
+			<h2 class="account-h2">Historique des commandes</h2>
+			@if ($past_orders->isEmpty())
+			<h3 class="account-h3">Vous n'avez pas de commande passées</h3>
+			@else
+			<table class="table table-bordered">
+				<tr class="active">
+					<th>N° de commande</th>
+					<th>Nom du destinataire</th>
+					<th>Adresse de livraison</th>
+					<th>Méthode de livraison</th>
+					<th>Coût total de la commande</th>
+					<th>Reçue le</th>
+				</tr>
+				@foreach ($past_orders as $past_order)
+				<tr>
+					<td>{{$past_order->id}}</td>
+					<td>{{$past_order->last_name}} {{$past_order->first_name}}</td>
+					<td>{{$past_order->address}}</td>
+					<td>{{$past_order->shipping_method}}</td>
+					<td>{{$past_order->total_price / 100}} €</td>
+					<td>{{$past_order->delivery_date / 100}}</td>
+				</tr>
+				@endforeach
+			</table>
+			@endif
+		</div>
 	</div>
 	<div>
 		<button>Supprimer mon compte</button>
