60358734e02eb5c00d3392d1a5e1e3f6f0075dc6
Nomilol <lezinaxel@gmail.com>
2017-03-31 12:30:59 +0200

    Ajout d'un placeholdit et changement de nom de route

---
+++ resources/views/cart.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="/css/app.css">
+    <title></title>
+  </head>
+  <body>
+<div class="container">
+  <div class="row">
+    <div class="col-md-6 col-md-offset-4">
+      <p>Récapitulatif du panier</p>
+    </div>
+  </div>
+  <div class="col-md-6 col-md-offset-3">
+    <div class="img">
+      <img src="http://placehold.it/150x150" alt="">
+    </div>
+  </div>
+</div>    
+  </body>
+</html>
