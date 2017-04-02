d507e7cfa0365449e19ad737a823f5580bb4aaa5
simplon <laure.milian@gmail.com>
2017-04-02 20:30:56 +0200

    Push les produits dans la session

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -11,11 +11,17 @@ class CartController extends Controller
     }
 
     public function addToCart(Request $request) {
+    	// if ($request->product_id === )
     	$product_id = $request->product_id;
 		$name = $request->name;
 		$price = $request->price;
 		$quantity = $request->quantity;
-		dd($quantity . $name . $price . $product_id);
+		$cart = [];
+		array_push($cart, ["product_id" => $product_id, "name" => $name, "price" => $price, "quantity" => $quantity]);
+		session(["cart" => $cart]);
+		dd(session("cart"));
+		$request->session()->flash('success', "Le produit a bien été ajouté au panier");
+        return back();
     }
 
     public function deleteFromCart() {
