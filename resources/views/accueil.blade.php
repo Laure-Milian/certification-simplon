6c21f6e65406e36b2931eb5a530950b15c31975a
Juliane Blier <blier.juliane@gmail.com>
2017-04-05 17:44:20 +0200

    Résolution coup de coeur du libraire

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -6,31 +6,33 @@
 
   <div class="container">
     <div class="row bloc1 col-12">
-      <div class="col-md-8 product-card">
-          <div class="card card-content">
-              <div class="image-wrapper-category">
-                  <a href="/{{ $sellerFavorite->id }}">
-                      <img src="{{ $sellerFavorite->picture }}" class="img-responsive" alt="Photo de {{ $sellerFavorite->name }}">
-                  </a>
-              </div>
-              <div class="caption caption-card">
-                  <h3 class="category-product-title"><a href="/{{ $sellerFavorite->id }}">{{ $sellerFavorite->name }}</a></h3>
+      <div class="col-md-8 product-card card">
 
-                  <div class="category-product-stock">
-                      <h4 class="pull-left"><strong>{{ $sellerFavorite->price / 100 }} €</strong></h4>
-                      @if ($sellerFavorite->stock === 0)
-                        <div class="text-danger pull-right">Indisponible</div>
-                      @else
-                        <form action="/add/cart" method="post" class="pull-right">
-                          <input type="hidden" name="product_id" value="{{$sellerFavorite->id}}">
-                          <input type="hidden" name="quantity" value="1">
-                          <button class="btn btn-success" type="submit"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Ajouter au panier</button>
-                        </form>
-                      @endif
-                  </div>
+          <div class="product-details row">
+            <div class="image-wrapper col-xs-6 col-md-6">
 
-              </div>
+                <img class="product-image img-responsive" src="http://placekitten.com/g/400/400" />
+
+            </div>
+            <div class="details col-xs-6 col-md-6">
+
+                <h2 class="product-title"> {{ $sellerFavorite->name }} </h2>
+                <p class="product-description"> {{ $sellerFavorite->description }} </p>
+                <h4 class="price"><strong> Prix : <span> {{ $sellerFavorite->price / 100 }} </span>€</strong></h4>
+
+                @if ($sellerFavorite->stock > 0)
+                <div class="add-to-cart">
+                  <form action="/add/cart" method="post">
+                      <input type="hidden" name="product_id" value="{{$sellerFavorite->id}}">
+                      <input type="hidden" name="quantity" value="1">
+                      <button class="btn btn-success" type="submit"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Ajouter au panier</button>
+                  </form>
+                </div>
+                @endif
+
+            </div>
           </div>
+
       </div>
       <div class="col-md-4">
         <div class="product-item">
